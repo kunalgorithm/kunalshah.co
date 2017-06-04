22c847222fb53eeef589806510e49891e27f2300
Kunal <kshah239@gmail.com>
2017-06-04 08:15:46 -0500

    new portfolio items

--- php/contact-form.php
+++ php/contact-form.php
@@ -22,7 +22,7 @@ if (array_key_exists('email', $_POST)) {
     //**DO NOT** use the submitter's address here as it will be forgery
     //and will cause your messages to fail SPF checks
 
-    $mail->setFrom('email@your-website.com', 'your-website.com'); //**Write here sender email. For example, emails will be sent to you from your website, so write email of your website (if you don't have it, write any email, which you want) and the name of your website. Example ('email@your-website.com', 'your-website.com')) Send from a fixed, valid address in your own domain, perhaps one that allows you to easily identify that it originated on your contact form**
+    $mail->setFrom('contact-form@kunalgorithm.com', 'kunalgorithm.com'); //**Write here sender email. For example, emails will be sent to you from your website, so write email of your website (if you don't have it, write any email, which you want) and the name of your website. Example ('email@your-website.com', 'your-website.com')) Send from a fixed, valid address in your own domain, perhaps one that allows you to easily identify that it originated on your contact form**
 
     //Send the message to yourself, or whoever should receive contact for submissions
 
